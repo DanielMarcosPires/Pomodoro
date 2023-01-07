cb14af34002776cf78e67024a72d86d45d204c5e
Daniel Marcos Pires <89848369+DanielMarcosPires@users.noreply.github.com>
2023-01-07 18:12:57 -0300

    Agora o tempo é configurável

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,8 @@
 const btn = document.querySelector('#start');
-const ticksound = document.querySelector('#tik')
-
 const valor = document.querySelector('input[type="time"]').value
-const [min = 25, sec = 30] = valor.split(':');
-
-
-
-function sound(){
-  
-  tickSound.currentTime = 0; // Volta para o início do áudio
-  tickSound.play(); // Reproduz o áudio
-}
+const [min, sec] = valor.split(":")
+parseInt(min)
+parseInt(sec)
 
 let counter = 1;
 let intervalo;  
@@ -20,13 +12,13 @@ function formataRelogio(numero) {
 }
 let cronometro = {
     seg: sec,
-    min: min
+    min:  min
 }
 
 function tempo() {
     cronometro.seg -= counter;
 
-    let porcentagem = (cronometro.min * 60 + cronometro.seg) / (23 * 60 + 30) * 100;
+    let porcentagem = (cronometro.min * 60 + cronometro.seg) / (25 * 60 + 30) * 100;
 
     let progresso = document.querySelector('#barraDeprogresso');
     progresso.style.setProperty('--v', porcentagem + '%');
@@ -34,15 +26,10 @@ function tempo() {
     let hora = document.querySelector('#hora');
     hora.innerHTML = `${formataRelogio(cronometro.min)}:${formataRelogio(cronometro.seg)}`;
 
-    ticksound.currentTime = 0; // Volta para o início do áudio
-    ticksound.play(); 
-    ticksound.volume = 0.2;
-
     if (cronometro.min === 0 && cronometro.seg === 0) {
         clearInterval(intervalo);
         alert('Acabou o tempo!');
         btn.removeAttribute('disabled',true);
-
     } else if (formataRelogio(cronometro.seg) >= 0) {
         
         if (formataRelogio(cronometro.seg) === formataRelogio(0) && formataRelogio(cronometro.min) > 0) {
